c315b0e4989bcbd475ca6eca4bc5fc02515976a2
unknown <vunw1@WGB01LT104866.worldwide.bbc.co.uk>
2015-03-30 14:43:39 +0100

    Added initial code base

---
+++ TrainingPlan/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TrainingPlan
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Services",
+                url: "Services",
+                defaults: new {controller = "Home", action = "Services", id = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
+                name: "AboutUs",
+                url: "AboutUs",
+                defaults: new {controller = "Home", action = "AboutUs", id = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
+                name: "ContactUs",
+                url: "ContactUs",
+                defaults: new {controller = "Home", action = "ContactUs", id = UrlParameter.Optional}
+                );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TrainingPlan/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingPlan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            @ViewBag.IsActiveLink = string.Empty;
+        }
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewBag.HomeIsActiveLink = "active";
+            ViewBag.Title = "BTB Training";
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.ServicesIsActiveLink = "active";
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            ViewBag.AboutUsIsActiveLink = "active";
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            ViewBag.ContactUsIsActiveLink = "active";
+            return View();
+        }
+	}
+}
